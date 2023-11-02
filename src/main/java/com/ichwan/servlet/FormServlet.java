f3f5d84e90cc4adcf6221678eea8c9679ffe7cf4
Ichwan <ichwansholihin03@gmail.com>
2023-11-02 21:15:13 +0700

    fixed: change upload location and getPath

--- src/main/java/com/ichwan/servlet/FormServlet.java
+++ src/main/java/com/ichwan/servlet/FormServlet.java
@@ -5,18 +5,19 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
 
 @WebServlet(urlPatterns = "/form")
 public class FormServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Path path = Path.of(Objects.requireNonNull(FormServlet.class.getResource("/html/form.html")).getPath());
+        Path path = new File(getClass().getResource("/html/form.html").getFile()).toPath();
         String html = Files.readString(path);
         resp.getWriter().println(html);
     }
@@ -24,7 +25,7 @@ public class FormServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("firstName") + " " + req.getParameter("lastName");
-        String hello  = "Semangat Pagi, "+name;
+        String hello = "Semangat Pagi, " + name;
         resp.getWriter().println(hello);
     }
 }
--- src/main/java/com/ichwan/servlet/FormUploadServlet.java
+++ src/main/java/com/ichwan/servlet/FormUploadServlet.java
@@ -1,22 +1,25 @@
 package com.ichwan.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @WebServlet(urlPatterns = "/form-upload")
+@MultipartConfig
 public class FormUploadServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Path path = Path.of(FormUploadServlet.class.getResource("/html/form-upload.html").getPath());
+        Path path = new File(getClass().getResource("/html/form-upload.html").getFile()).toPath();
         String html = Files.readString(path);
         resp.getWriter().println(html);
     }
@@ -27,9 +30,21 @@ public class FormUploadServlet extends HttpServlet {
         String name = req.getParameter("name");
         Part profile = req.getPart("profile");
 
-        Path uploadLocation = Path.of("/upload" + profile.getSubmittedFileName());
+        Path uploadLocation = Path.of("upload/" + profile.getSubmittedFileName());
         Files.copy(profile.getInputStream(), uploadLocation);
 
-        resp.getWriter().println("hello "+name+", your profile pic in "+uploadLocation.toAbsolutePath());
+        String html = """
+                <html>
+                <body>
+                Name : $name
+                <br>
+                Profile : <img width="400px" height="400px" src="/download?file=$profile" />
+                </body>
+                <html>
+                """
+                .replace("$name", name)
+                .replace("$profile", uploadLocation.getFileName().toString());
+
+        resp.getWriter().println(html);
     }
 }
